65068a3bd10588868ddac3a923a68ab21db66e8a
LLapuras <chenwanwan13@gmail.com>
2020-08-13 14:58:34 +0800

    备份

--- Assets/Scenes/Script/LSystem.cs
+++ Assets/Scenes/Script/LSystem.cs
@@ -5,16 +5,32 @@ using System.Text;
 
 public class LSystem : MonoBehaviour
 {
-    [Header("Input Setting")]
+    
     public Dictionary<char, string> rules = new Dictionary<char, string>();
+
+    [Header("Plant Units")]
+    public GameObject cylinder;
+    //public GameObject stem;
+    //public GameObject petal;
+    //public GameObject leaf;
+
+    [Header("Input Setting")]
     [Range(0, 6)]
     public int iterations = 4; //大于x的话Unity会炸，在我的电脑上x=8(仅供参考
     public string input = "F";
     public string rule = "F+F+F+F";
     public float angle = 45;
     [Range(0.9f, 1.1f)]
-    public float scalingfactor = 1;
-    public float height = 1;
+    public float scalingFactor = 1;
+
+    [Header("Length Setting")]
+    [Range(-5f, 5f)]
+    public float inputHeight = 1;
+    [Range(-5f, 5f)]
+    public float stepHeight = 1;
+    [Range(-5f, 5f)]
+    public float openScale = 1;//开合程度
+
 
     [Header("Random Setting")]
     public bool isRandom = false;
@@ -23,11 +39,6 @@ public class LSystem : MonoBehaviour
     [Header("Result")]
     public string result;
 
-    [Header("Plant Units")]
-    public GameObject cylinder;
-    public GameObject stem;
-    public GameObject petal;
-    public GameObject leaf;
 
     [Header("Debug")]
     public bool debug = true;
@@ -83,18 +94,19 @@ public class LSystem : MonoBehaviour
         return sb.ToString();
     }
 
+    
     struct point
     {
-        public point(Vector3 rP, Vector3 rA, float rL) { Point = rP; Angle = rA; BranchLength = rL; }
-        public Vector3 Point;
+        public point(Vector3 rP, Vector3 rA, float rL) { Pos = rP; Angle = rA; BranchLength = rL; }
+        public Vector3 Pos;
         public Vector3 Angle;
-        public float BranchLength;
+        public float BranchLength;//petal的间隔高度
     }
 
     void determinePoints(string p_input)
     {
         Stack<point> returnValues = new Stack<point>();
-        point lastPoint = new point(transform.position, transform.position, 1f);//初始位置和父物体关联
+        point lastPoint = new point(new Vector3(transform.position.x, transform.position.y + stepHeight, transform.position.z), transform.eulerAngles, inputHeight);//初始位置和父物体关联
         returnValues.Push(lastPoint);
 
         foreach (char c in p_input)
@@ -104,8 +116,8 @@ public class LSystem : MonoBehaviour
                 case 'F': 
                     points.Add(lastPoint);
 
-                    point newPoint = new point(lastPoint.Point + new Vector3(0, lastPoint.BranchLength, 0), lastPoint.Angle, 1f);
-                    newPoint.BranchLength = lastPoint.BranchLength*scalingfactor;
+                    point newPoint = new point(lastPoint.Pos + new Vector3(0, lastPoint.BranchLength, 0), lastPoint.Angle, inputHeight);
+                    newPoint.BranchLength = lastPoint.BranchLength * scalingFactor;
                     if (newPoint.BranchLength <= 0.0f) newPoint.BranchLength = 0.001f;
 
                     newPoint.Angle.y = lastPoint.Angle.y;
@@ -116,8 +128,10 @@ public class LSystem : MonoBehaviour
                         newPoint.Angle.y += UnityEngine.Random.Range(randomRange.x, randomRange.y);
                     }
 
-                    newPoint.Point = pivot(newPoint.Point, lastPoint.Point, new Vector3(newPoint.Angle.x, 0, 0));
-                    newPoint.Point = pivot(newPoint.Point, lastPoint.Point, new Vector3(0, newPoint.Angle.y, 0));
+                    newPoint.Pos = pivot(newPoint.Pos, lastPoint.Pos, new Vector3(newPoint.Angle.x, 0, 0));
+                    newPoint.Pos = pivot(newPoint.Pos, lastPoint.Pos, new Vector3(0, newPoint.Angle.y, 0));
+                    //newPoint.Point = pivot(newPoint.Point, lastPoint.Point, Vector3.zero);
+                    //newPoint.Point = pivot(newPoint.Point, lastPoint.Point, Vector3.zero);
 
                     points.Add(newPoint);
                     lastPoint = newPoint;
@@ -155,10 +169,11 @@ public class LSystem : MonoBehaviour
     {
         for (int i = 0; i < points.Count; i += 2)
         {
-            CreateCylinder(points[i], points[i + 1], 1f);
+            CreateCylinder(points[i], points[i + 1], 1f, cylinder);
         }
     }
 
+    //
     Vector3 pivot(Vector3 point1, Vector3 point2, Vector3 angles)
     {
         Vector3 dir = point1 - point2;
@@ -167,22 +182,23 @@ public class LSystem : MonoBehaviour
         return point1;
     }
 
-    void CreateCylinder(point point1, point point2, float radius)
+    void CreateCylinder(point point1, point point2, float radius, GameObject go)
     {
-        GameObject newCylinder = (GameObject)Instantiate(cylinder);
-        newCylinder.SetActive(true);
-        float length = Vector3.Distance(point2.Point, point1.Point);
-        radius = radius * length;
+        GameObject newGo = (GameObject)Instantiate(go);
+        newGo.SetActive(true);
+        float length = Vector3.Distance(point2.Pos, point1.Pos);
+        //radius = radius * length;
+        Debug.Log(length);
 
-        Vector3 scale = new Vector3(radius, length, radius);
-        newCylinder.transform.localScale = scale;
+        Vector3 scale = new Vector3(radius, openScale, radius);
+        newGo.transform.localScale = scale;
 
-        newCylinder.transform.position = point1.Point;
-        newCylinder.transform.Rotate(point2.Angle);
+        newGo.transform.position = point1.Pos;
+        newGo.transform.Rotate(point2.Angle);
 
-        newCylinder.transform.parent = this.transform;
+        newGo.transform.parent = this.transform;
 
-        branches.Add(newCylinder);
+        branches.Add(newGo);
     }
 
     void Update()
@@ -191,7 +207,7 @@ public class LSystem : MonoBehaviour
         {
             for (int i = 0; i < points.Count; i += 2)
             {
-                Debug.DrawLine(points[i].Point, points[i + 1].Point, Color.red);
+                Debug.DrawLine(points[i].Pos, points[i + 1].Pos, Color.red);
             }
         }
     }
